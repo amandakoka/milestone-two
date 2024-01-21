f606f1891d5133c8a6cb8111355153df01e6723f
amandakoka <amandakoka@icloud.com>
2024-01-21 20:53:23 +0000

    fix and add code for the increment of scores

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -49,13 +49,17 @@ const questions = [
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answerbuttons");
 const nextButton = document.getElementById("nextbtn");
+const correctScoreElement = document.getElementById("score");
+const incorrectScoreElement = document.getElementById("incorrect");
 
 let currentQuestionIndex = 0;
-let score = 0;
+let correctScore = 0;
+let incorrectScore = 0;
 
 function startQuiz() {
     currentQuestionIndex = 0;
-    score = 0;
+    correctScore = 0;
+    incorrectScore = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
 }
@@ -72,14 +76,16 @@ function showQuestion() {
         button.classList.add("btn");
         answerButtons.appendChild(button);
         if(answer.correct){
-            button.dataset.correct = answer.correct;
+            button.dataset.correct = answer.correct;    
         }
         button.addEventListener("click", selectAnswer);
     });
 }
 
+
 function resetState() {
     nextButton.style.display = "none";
+    questionElement.src = ``;
     while(answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
     }
@@ -90,9 +96,12 @@ function selectAnswer(e) {
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
-        score++;
+        correctScore++;
+        correctScoreElement.textContent = correctScore;
     }else{
         selectedBtn.classList.add("incorrect");
+        incorrectScore++;
+        incorrectScoreElement.textContent = incorrectScore;
     }
     Array.from(answerButtons.children).forEach(button => {
         if(button.dataset.correct === "true"){
@@ -103,9 +112,9 @@ function selectAnswer(e) {
     nextButton.style.display = "block";
 }
 
+
 function showScore() {
     resetState();
-    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!!`;
     nextButton.innerHTML = "Play again!";
     nextButton.style.display = "block";
 }
